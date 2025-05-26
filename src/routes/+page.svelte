c9b3dc94cbbeb02c30eab0337ce07cdc7aed6728
NeoSahadeo <neosahadeo@protonmail.com>
2025-05-26 02:17:34 +0100

    :bug: Font size fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="hero min-h-screen">
 	<div class="hero-content text-center">
 		<div class="flex max-w-md flex-col gap-3">
-			<h1 class="text-5xl font-bold">/~$ Neo Sahadeo</h1>
+			<h1 style="font-size: 2em !important;" class="text-5xl font-bold">/~$ Neo Sahadeo</h1>
 			{#if !loaded}
 				<div class={`skeleton ${shared_img_c}`}></div>
 				<!-- <div class="skeleton h-4 w-full"></div> -->
@@ -25,7 +25,9 @@
 			/>
 			{#if loaded}
 				<div class="flex gap-2"></div>
-				<p class="w-full overflow-hidden text-xl text-ellipsis">Hello Everynan!</p>
+				<p class="w-full overflow-hidden text-xl text-ellipsis" style="font-size: 2em !important;">
+					Hello Everynan!
+				</p>
 			{/if}
 		</div>
 	</div>
